cb18b129e0faea1f49ae2286c3ddf312550c3b42
Minh Bui <btminh.dev@gmail.com>
2026-04-03 09:19:33 +0700

    update to use transaction pooler

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,12 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql:///./test.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=5,
+    max_overflow=10,
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
